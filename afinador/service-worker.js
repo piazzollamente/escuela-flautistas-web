666db037aa0ee06751498d8d01087d03d9b64dfa
piazzollamente <valen.galaz@gmail.com>
2026-07-04 15:58:56 -0400

    Actualizar caché del afinador

--- afinador/service-worker.js
+++ afinador/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "afinador-edf-v14";
+const CACHE_NAME = "afinador-edf-v15";
 const APP_SHELL = [
   "./",
   "index.html",
@@ -32,7 +32,6 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
-
   const url = new URL(event.request.url);
   if (url.origin !== self.location.origin) return;
 
